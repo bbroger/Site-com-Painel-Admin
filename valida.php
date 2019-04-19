86bbe1fa823e1472e071ea9ada7436a5f9dbb81a
Rogerio Santos <bb.roger@hotmail.com>
2019-04-19 17:41:04 -0300

    ajuste na página valida.php

--- valida.php
+++ valida.php
@@ -8,31 +8,36 @@
         //captura as informações enviadas via POST
         $usuario = filter_input(INPUT_POST, 'usuario', FILTER_SANITIZE_STRING);
         $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+        //var_dump($senha);
+        //exit;
         
         //verifica se os campos usuario e senha foram preenchidos
         if((!empty($usuario) && (!empty($senha)))){
             //gerar senha criptografada
-            //echo password_hash($senha, PASSWORD_DEFAULT);
+            //$password = password_hash($senha, PASSWORD_DEFAULT);
+            //echo $password;
+            //exit;
             //Pesquisar o usuário no BD
-            $result_usuario = "SELECT id, name, email, password FROM users WHERE email = $usuario LIMIT 1";
+            $result_usuario = "SELECT id, name, email, password FROM users WHERE email = '$usuario' LIMIT 1";
             $resultado_usuario = mysqli_query($conn, $result_usuario);
 
             if($resultado_usuario){
                 $row_usuario =  mysqli_fetch_assoc($resultado_usuario);
 
-            var_dump($row_usuario['email']);
-            var_dump($row_usuario['senha']);
-            exit;
+                //var_dump(password_get_info($row_usuario['password']));
+                //exit;
 
-                if(($usuario === $row_usuario['email']) && (password_verify($senha, $row_usuario['password']))){
+                if((password_verify($senha, $row_usuario['password']))){
 
+                    header("Location: administrativo.php");
 
                 }else{
-                    $_SESSION['msg'] = "Usuário ou senha incorretos, tente novamente.";
+                    $_SESSION['msg'] = "1 - Usuário ou senha incorretos, tente novamente.";
                     header("Location: login.php");
                 }
             }else{
-                    $_SESSION['msg'] = "Usuário ou senha incorretos, tente novamente.";
+                    $_SESSION['msg'] = "2 - Usuário ou senha incorretos, tente novamente.";
                     header("Location: login.php");
             }
 
